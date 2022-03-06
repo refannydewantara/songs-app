05ee8cbe4f7dcb82397e85b41ee550bad31ec430
alpredovandy <alpredovandy@gmail.com>
2022-03-06 10:33:11 +0700

    fixed all

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {initialStateAuth, reducerAuth, AuthContext} from "./context/authContext.";
+import Header from "./components/Header";
+import Login from "./components/Login";
+import Home from "./components/Home";
 
 function App() {
+  const [state, dispatch] = React.useReducer(reducerAuth, initialStateAuth);
+
+  React.useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('user') || null)
+    const token = JSON.parse(localStorage.getItem('token') || null)
+
+    if(user && token){
+      dispatch({
+        type: 'LOGIN',
+        payload: {
+          user,
+          token
+        }
+      })
+    }
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <AuthContext.Provider
+          value={{
+            state,
+            dispatch
+          }}
+      >
+        <Header />
+        <div className="App">{!state.isAuthenticated ? <Login /> : <Home />}</div>
+      </AuthContext.Provider>
   );
 }
 
